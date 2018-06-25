760a8a513321f1cdbc05e771aa6cfecd09feb1a5
Patryk <xmorswinx@gmail.com>
2018-06-25 18:01:35 +0200

    Create support for dots in hrefs

--- lib/router/router.php
+++ lib/router/router.php
@@ -50,7 +50,7 @@ function getRegex($pattern){
     $pattern = preg_replace('#\(/\)#', '/?', $pattern);
 
     // Create capture group for ":parameter"
-    $allowedParamChars = '[a-zA-Z0-9\_\-]+';
+    $allowedParamChars = '[a-zA-Z0-9\_\-.]+';
     $pattern = preg_replace(
         '/:(' . $allowedParamChars . ')/',   # Replace ":parameter"
         '(?<$1>' . $allowedParamChars . ')', # with "(?<parameter>[a-zA-Z0-9\_\-]+)"
